0cd887b9bfc063dc77657498efd2321d7442912a
dialvec <diavelandiaca@unal.edu.co>
2022-02-14 02:16:09 +0100

    crash fix

--- RFunctions/DataSource.R
+++ RFunctions/DataSource.R
@@ -1,6 +1,6 @@
 URL_POSITIVE_CASES  <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
-URL_DECEASES_CASES  <- "https://github.com/CSSEGISandData/COVID-19/blob/1dd60c4dfe9e89429f1c185a5ecc20d36c5e2461/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+URL_DECEASES_CASES  <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/1dd60c4dfe9e89429f1c185a5ecc20d36c5e2461/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
 URL_RECOVERED_CASES <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/1dd60c4dfe9e89429f1c185a5ecc20d36c5e2461/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
